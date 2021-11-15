04cb9d8b425f4731ef8845d96499096a1ed0fd7a
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2021-11-15 15:47:31 -0500

    tweak minor styling and begin writing functions

--- calculator.js
+++ calculator.js
@@ -9,29 +9,40 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+
 function getCurrentUIValues() {
-  return {
+  const values =  {
     amount: +(document.getElementById("loan-amount").value),
     years: +(document.getElementById("loan-years").value),
     rate: +(document.getElementById("loan-rate").value),
   }
+  return values;
 }
 
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  let amount = document.getElementById("loan-amount").value = 5000;
+  let years = document.getElementById("loan-years").value = 3;
+  let rate = document.getElementById("loan-rate").value = 7.5;
+  calculateMonthlyPayment();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  getCurrentUIValues();
+  
+  updateMonthly();
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let monthlyPayment = (values.amount * (values.rate / 100)) / (1 - ((1 + values.rate / 100)** (-1 * (12 * values.years))))
+  console.log(monthlyPayment)
 }
 
 // Given a string representing the monthly payment value,
