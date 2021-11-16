b486c2153b4b296d3a3b0ac2a8c161603bdef9e3
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2021-11-16 09:52:14 -0500

    test monthly payment function

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,13 @@
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
-});
+describe('calculate monthly payment', function () {
+  it('should calculate the monthly rate correctly', function () {
+    expect(parseFloat(calculateMonthlyPayment({amount: 1000, years: 2, rate: 3}))).toEqual(42.98);
+    expect(parseFloat(calculateMonthlyPayment({amount: 0, years: 2, rate: 3}))).toEqual(0);
+  });
+  it("should return a result with 2 decimal places", function() {
+    expect(calculateMonthlyPayment({amount: 1000, years: 2, rate: 3})).toEqual('42.98');
+    expect(calculateMonthlyPayment({amount: 0, years: 2, rate: 3})).toEqual('0.00');
+  });
+})
 
 
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
